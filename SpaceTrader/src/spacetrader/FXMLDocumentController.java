ccb33709df0c0f769d83c1bc74fbed4b475d36ea
olahay3 <shirotsubame@gmail.com>
2014-09-16 15:37:48 -0400

    Olivia is trying to get fx stuff to work..

--- SpaceTrader/src/spacetrader/FXMLDocumentController.java
+++ SpaceTrader/src/spacetrader/FXMLDocumentController.java
@@ -3,40 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package spacetrader;
 
-import impl.org.controlsfx.i18n.Localization;
-import impl.org.controlsfx.i18n.Translations;
-import java.awt.Dialog;
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import org.controlsfx.control.action.Action;
-import static org.controlsfx.dialog.Dialog.Actions.NO;
-import static org.controlsfx.dialog.Dialog.Actions.YES;
-import org.controlsfx.dialog.Dialog.Actions;
-import org.controlsfx.dialog.Dialogs;
-//import org.controlsfx.dialog.Dialog.Actions;
-//import org.controlsfx.dialog.Dialog;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
 
 /**
  * FXML Controller class
  *
- * @author Olivia
+ * @author shiro_000
  */
 public class FXMLDocumentController implements Initializable {
     @FXML
@@ -46,14 +34,11 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label label;
     @FXML
-    private Button loadGameButton;
-    @FXML
     private Button newGameButton;
     @FXML
-    private Button optionsButton;
+    private Button loadGameButton;
     @FXML
-    private FXMLLoader loader = new FXMLLoader();
-   
+    private Button optionsButton;
 
     /**
      * Initializes the controller class.
@@ -63,51 +48,28 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
 
-
     @FXML
     private void handleNewGameButtonAction(ActionEvent event) {
-        //launches a simple dialouge box and collects user input. Just testing out
-        //the controlsFX API
-        
-        //as far as I can tell we should be able to make the dialog box contents
-        //in a seperate file, create it as a new content object, and launch it that way
-        //api here: http://controlsfx.bitbucket.org/org/controlsfx/dialog/Dialog.html
-        Action response = Dialogs.create()
-                .title("New Game")
-                .masthead("Confirm new game?")
-                //.message( "")
-                //.actions(Dialog.Actions.OK, Dialog.Actions.CANCEL)
-                .showConfirm();
-        // Should direct to a new screen upon hitting "yes" but it doesn't work. Anyone have ideas?
-        if (response == org.controlsfx.dialog.Dialog.Actions.YES) {
-            URL url = getClass().getResource("CharacterCreation.fxml");
-            loader.setLocation(url);
-            loader.setBuilderFactory(new JavaFXBuilderFactory());
-            try {
-             loadingPane = (AnchorPane) loader.load(url.openStream());
-             } catch (IOException ex) {
-              Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            
+        Parent root;
+        try {
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("CharacterCreation.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("New Commander");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
+
+        //hide this current window (if this is whant you want
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
         }
-        //loadingPane.getChildren().setAll(FXMLLoader.load(filename));
-     
     }
-    
-        @FXML
+
+    @FXML
     private void handleLoadButtonAction(ActionEvent event) {
-        Action response = Dialogs.create()
-                .title("Load Game")
-                .masthead("Load last saved game?")
-                .showConfirm();
     }
 
     @FXML
     private void handleOptionButtonAction(ActionEvent event) {
-        Action response = Dialogs.create()
-                .title("Options")
-                .showConfirm();
     }
     
 }
--- SpaceTrader/src/spacetrader/SpaceTrader.java
+++ SpaceTrader/src/spacetrader/SpaceTrader.java
@@ -8,33 +8,38 @@ package spacetrader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author Sarah
  */
+
 public class SpaceTrader extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        //TitleScreen scene = new TitleScreen();
-        //scene.start(stage);
-        stage.show();
-    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(SpaceTrader.class, (java.lang.String[])null);
     }
-    
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+//                        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+//            loader.setController(new FXMLDocumentController());
+//            Pane mainPane = loader.load();
+            StackPane page = (StackPane) FXMLLoader.load(SpaceTrader.class.getResource("FXMLDocument.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("SpaceTrader");
+            primaryStage.show();
+        } catch (Exception ex) {
+            
+        }
+    }
 }
