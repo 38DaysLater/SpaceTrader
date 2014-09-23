124ad4f37e33896f70ae86dc2bc2e4d5906673e4
Afiq Azaibi <afiq.azaibi@gmail.com>
2014-09-23 14:33:50 -0400

    Universe has successful toString method. Also fixing some bugs in Universe.

--- SpaceTrader/src/spacetrader/Universe.java
+++ SpaceTrader/src/spacetrader/Universe.java
@@ -5,23 +5,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
+/**
+ *
+ * @author AfiqAzaibi
+ */
 
 public class Universe {
         private final int NUM_SOLAR_SYSTEMS = 5;
         SolarSystem[] SSArray = new SolarSystem[NUM_SOLAR_SYSTEMS];
         
+        
+        
+        /**
+        * Constructor of Solar System 
+        * reads in text file and randomly generates solar system coordinates
+        * @param none
+        * @return none
+        * 
+        */
 	public Universe() throws IOException{
-		//set up total number of SS we want
-		
 		//sets up string of solar system name creation
 		String[] solarSystemNames = new String[NUM_SOLAR_SYSTEMS];
-		FileReader inputFile = new FileReader("SolarSystemNames.txt");
-        BufferedReader bufReader = new BufferedReader(inputFile);
-        
-		for(int i = 0; i < NUM_SOLAR_SYSTEMS; i ++){
+                try {
+                    FileReader inputFile = new FileReader("SolarSystemNames.txt");
+                    BufferedReader bufReader = new BufferedReader(inputFile);
+            
+                    for(int i = 0; i < NUM_SOLAR_SYSTEMS; i ++){
 			solarSystemNames[i] = bufReader.readLine();
-		}
+                    }
+                    
+                    bufReader.close();
+                }
+                
+                catch(IOException e){
+                    System.out.println("goofed");
+                }
+        
 
 		//randomly generates a solar system name and location such that no two occupy the same space		
 		HashSet<int[]> coordinates = new HashSet<int[]>();
@@ -55,14 +74,15 @@ public class Universe {
                         SSArray[i] = ss;
 			i++;
 		}
-		
-        bufReader.close();
-
-        
 	}//ends the constructor
 
         
         @Override
+        /**
+        * Displays the contents of the solar system 
+        * @param none
+        * @return a string of the contents
+        */
 	public String toString(){
             String returnString = "";
             for(int i = 0; i < NUM_SOLAR_SYSTEMS; i ++){
@@ -71,13 +91,7 @@ public class Universe {
              
             return returnString;
 	}
-	public static void main(String args[]) throws IOException{
-		Universe u = new Universe();
-                System.out.println();
-                System.out.print(u);
-                
-			
-	}
+
 	
 	
 }
