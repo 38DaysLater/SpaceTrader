aff445fbbe509d6f41a0a03cbb8f6ffa379f78d9
Afiq Azaibi <afiq.azaibi@gmail.com>
2014-10-19 14:38:30 -0400

    added ship types.

--- SpaceTrader/src/spacetrader/Ships.java
+++ SpaceTrader/src/spacetrader/Ships.java
@@ -11,10 +11,101 @@
  */
 package spacetrader;
 
+import java.util.ArrayList;
+
 /**
  *This is where all the ships in the game will go. 
  * @author AfiqAzaibi
  */
 public class Ships {
+    
+    
+        private static ArrayList<Ship> list;
+    
+    static {
+        list = new ArrayList<Ship>();
+    //    public Ship(String name, int price, int MLP, int weight, int attack, int speed, int fuelCapacity){
+        Ship tankShipOne = new Ship("Titan 1", 1000, 2, 70, 5, 4, 600);
+        list.add(tankShipOne);
+        
+        Ship tankShipTwo = new Ship("Titan 2", 2000, 5, 85, 7, 4, 750);
+        list.add(tankShipTwo);
+
+        Ship tankShipThree = new Ship("Titan 3", 5000, 100, 10, 10, 5, 1000);
+        list.add(tankShipThree);        
+        
+        
+        
+        Ship speedShipOne = new Ship("Banshee 1", 1000, 2, 25, 5, 5, 600); 
+        list.add(speedShipOne);
+
+        Ship speedShipTwo = new Ship("Banshee 2", 2000, 5, 15, 5, 7, 750);
+        list.add(speedShipTwo);
+
+        Ship speedShipThree = new Ship("Banshee 3", 5000, 10, 5, 5, 10, 1000);
+        list.add(speedShipThree);        
+        
+        
+        
+        //ships that for people who don't invest in pilot
+        Ship normalShipOne = new Ship("Rusty 1", 1000, 0, 50, 3, 4, 500);
+        list.add(normalShipOne);
+        
+        Ship normalShipTwo = new Ship("Rusty 2", 2000, 0, 30, 4, 5, 700);
+        list.add(normalShipTwo);
+
+        
+        
+        Ship tradeShipOne = new Ship("Serenity 1", 1000, 0, 70, 5, 3, 600);
+        list.add(tradeShipOne);
+
+        Ship tradeShipTwo = new Ship("Serenity 2", 2000, 0, 75, 5, 2, 700);
+        list.add(tradeShipTwo);        
+        
+        
+    }
+    
+    public static ArrayList<Ship> getList() {
+        return list;
+    }
+   
+    public static Ship getShip(String name) {
+        if (name.equals("HeavyOne")) {
+            return list.get(0);
+        } else if(name.equals("HeavyTwo")){
+            return list.get(1);
+         } else if (name.equals("HeavyThree")){
+             return list.get(2);
+         } else if (name.equals("FastOne")){
+             return list.get(3);
+         } else if (name.equals("FastTwo")){
+             return list.get(4);
+         } else if (name.equals("FastThree")){
+             return list.get(5);
+         } else if (name.equals("NormalOne")){
+             return list.get(6);
+         } else if (name.equals("NormalTwo")){
+             return list.get(7);
+         } else if (name.equals("TradeOne")){
+             return list.get(8);
+         } else if (name.equals("TradeTwo=")){
+             return list.get(9);
+         } else {
+             return null;
+         }
+    }
+    
+    /*
+    public static ArrayList<Item> getElligibleItems(int tl) {
+        ArrayList<Item> newList = new ArrayList<Item>();
+        for (int i = 0; i <= tl; i++) {
+            newList.add(list.get(i));
+        }
+        return newList;
+    }
+    */
+
+    
+    
     
 }
