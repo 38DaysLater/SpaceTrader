e473c0385404508669e207d1c4e8f85bd3a769be
Afiq Azaibi <afiq.azaibi@gmail.com>
2014-10-29 17:17:14 -0400

    Ship- capacity is implemented Character- when ship is set, the character's inventory gets changed. Ships- modified constructor

--- SpaceTrader/src/spacetrader/Ships.java
+++ SpaceTrader/src/spacetrader/Ships.java
@@ -25,41 +25,41 @@ public class Ships {
     static {
         list = new ArrayList<Ship>();
     //    public Ship(String name, int price, int MLP, int weight, int attack, int speed, int fuelCapacity, int health){
-        Ship tankShipOne = new Ship("Titan 1", 1000, 2, 70, 5, 4, 600, 100);
+        Ship tankShipOne = new Ship("Titan 1", 1000, 2, 70, 5, 4, 600, 100, 10);
         list.add(tankShipOne);
         
-        Ship tankShipTwo = new Ship("Titan 2", 2000, 5, 85, 7, 4, 750, 125);
+        Ship tankShipTwo = new Ship("Titan 2", 2000, 5, 85, 7, 4, 750, 125, 20);
         list.add(tankShipTwo);
 
-        Ship tankShipThree = new Ship("Titan 3", 5000, 100, 10, 10, 5, 1000, 150);
+        Ship tankShipThree = new Ship("Titan 3", 5000, 100, 10, 10, 5, 1000, 150, 50);
         list.add(tankShipThree);        
         
         
         
-        Ship speedShipOne = new Ship("Banshee 1", 1000, 2, 25, 5, 5, 600, 40); 
+        Ship speedShipOne = new Ship("Banshee 1", 1000, 2, 25, 5, 5, 600, 40, 10); 
         list.add(speedShipOne);
 
-        Ship speedShipTwo = new Ship("Banshee 2", 2000, 5, 15, 5, 7, 750, 50);
+        Ship speedShipTwo = new Ship("Banshee 2", 2000, 5, 15, 5, 7, 750, 50, 20);
         list.add(speedShipTwo);
 
-        Ship speedShipThree = new Ship("Banshee 3", 5000, 10, 5, 5, 10, 1000, 60);
+        Ship speedShipThree = new Ship("Banshee 3", 5000, 10, 5, 5, 10, 1000, 60, 30);
         list.add(speedShipThree);        
         
         
         
         //ships that for people who don't invest in pilot
-        Ship normalShipOne = new Ship("Shiny 1", 1000, 0, 50, 3, 4, 500, 70);
+        Ship normalShipOne = new Ship("Shiny 1", 1000, 0, 50, 3, 4, 500, 70, 20);
         list.add(normalShipOne);
         
-        Ship normalShipTwo = new Ship("Shiny 2", 2000, 0, 30, 4, 5, 700, 80);
+        Ship normalShipTwo = new Ship("Shiny 2", 2000, 0, 30, 4, 5, 700, 80, 40);
         list.add(normalShipTwo);
 
         
         
-        Ship tradeShipOne = new Ship("Serenity 1", 1000, 0, 70, 5, 3, 600, 60);
+        Ship tradeShipOne = new Ship("Serenity 1", 1000, 0, 70, 5, 3, 600, 60, 100);
         list.add(tradeShipOne);
 
-        Ship tradeShipTwo = new Ship("Serenity 2", 2000, 0, 75, 5, 2, 700, 70);
+        Ship tradeShipTwo = new Ship("Serenity 2", 2000, 0, 75, 5, 2, 700, 70, 200);
         list.add(tradeShipTwo);        
         
         
