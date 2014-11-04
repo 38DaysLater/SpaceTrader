414eb59a1737273e84bb4d0a54f43e3171cf98b7
Afiq Azaibi <afiq.azaibi@gmail.com>
2014-11-03 23:08:36 -0500

    Shipyard- upgrade from 1 level to next is fixed.

--- SpaceTrader/src/spacetrader/ShipYard.java
+++ SpaceTrader/src/spacetrader/ShipYard.java
@@ -129,8 +129,6 @@ public class ShipYard implements Serializable{
         String upgradeShipType = upgradeShipName.substring(0);
         int upgradeShipLevel = Integer.parseInt(upgradeShipName.substring(lengthNew - 1));
         
-        System.out.println(currentShipLevel);
-        System.out.println(upgradeShipLevel);
         
         
         
@@ -143,11 +141,11 @@ public class ShipYard implements Serializable{
             return "You have too much cargo on your current ship to get this "
                     + "new one. Sell your merchandise or pick a ship that can "
                     + "accomodate your cargo";
-        } /*else if (currentShipLevel <= upgradeShipLevel - 1) {
+        } else if (currentShipLevel < upgradeShipLevel - 1) {
             return "Your current ship level is " + currentShipLevel + ". "
                     + "You must upgrade to level " + (currentShipLevel + 1) + 
                     " before upgrading to level " + upgradeShipLevel;
-        } */
+        } 
         
 
        //gives refund on current ship and deducts old one. 
