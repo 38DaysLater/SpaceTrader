cee9bb7c8971db10759c6fd5e6a7b36d630814a0
alixp_000 <alixp_000@Hutchinson>
2014-11-13 01:06:03 -0500

    MarketSellItemTest.java - Hutchinson

--- SpaceTrader/test/spacetrader/InventoryTest.java
+++ SpaceTrader/test/spacetrader/InventoryTest.java
@@ -15,12 +15,12 @@ import java.util.Random;
  * @author lsmoore
  */
 public class InventoryTest {
-    
+
     Inventory inventory;
     ArrayList<String> elligibleItems;
     ArrayList<String> notElligible;
     int techLevel;
-    
+
     @Before
     public void setUp() {
         techLevel = new Random().nextInt(8);
@@ -32,9 +32,9 @@ public class InventoryTest {
             if (!elligibleItems.contains(name)) {
                 notElligible.add(name);
             }
-        }   
+        }
     }
-    
+
 
     /**
      * Test 1 of the add method. Checks basic information
@@ -52,7 +52,7 @@ public class InventoryTest {
         assertEquals(inventory.getItemCount("Robots"), -1);
         assertEquals(inventory.getItemCount("Name not in the list"), -1);
     }
-    
+
     /**
      * Test 2 of the add method. Checks that the capacity is enforced
      */
@@ -60,29 +60,29 @@ public class InventoryTest {
     public void testAdd2() {
         inventory.add("Water");
         inventory.add("Ore");
-        
+
         for (int i = 0; i < 100; i++) {
             inventory.add("Furs");
         }
-        
+
         assertEquals(inventory.getItemCount("Water"), 1);
         assertEquals(inventory.getItemCount("Ore"), 1);
         assertEquals(inventory.getItemCount("Furs"), 98);
-        
+
         if (elligibleItems.contains("Robots")){
             assertEquals(inventory.getItemCount("Robots"), 0);
         }
         assertEquals(inventory.getItemCount("Robots"), -1);
         assertEquals(inventory.getItemCount("Name not in the list"), -1);
     }
-    
+
     /**
      * Test 3 of the add method.  Checks when multiple items are added
      */
     @Test
     public void testAdd3() {
         inventory.add("Food", 15);
-        
+
         if (elligibleItems.contains("Medicine")){
             inventory.add("Medicine", 31);
             assertEquals(31, inventory.getItemCount("Medicine"));
@@ -91,7 +91,7 @@ public class InventoryTest {
         assertEquals(inventory.getItemCount("Food"), 15);
         assertEquals(inventory.getItemCount("Not there"), -1);
     }
-    
+
     /**
      * Test 4 of the add method.  Checks what happens when nothing is added.
      */
@@ -100,12 +100,12 @@ public class InventoryTest {
         for (String items: elligibleItems) {
             assertEquals(inventory.getItemCount(items), 0);
         }
-        
+
         for (String items: notElligible) {
             assertEquals(inventory.getItemCount(items), -1);
         }
     }
-    
+
     /**
      * Test 5 of the add method.  Checks the inventory when illegible items are added
      */
@@ -116,7 +116,7 @@ public class InventoryTest {
             assertEquals(-1, inventory.getItemCount(item));
         }
     }
-    
+
     /**
      * Test 6 of the add method.  Checks the inventory when null is added
      */
@@ -127,29 +127,29 @@ public class InventoryTest {
         inventory.add(null);
         inventory.add(null, 7);
         inventory.add("Food", 0);
-        
+
         assertEquals(4, inventory.getItemCount("Water"));
         assertEquals(0, inventory.getItemCount("Food"));
     }
-    
+
     /**
      * Test 7 of the add method.  Additional null testing
      */
     @Test
     public void testAdd7() {
         inventory.add(null, 100);
-        
+
         assertEquals(-1, inventory.getItemCount(null));
-        
+
         for (String items: elligibleItems) {
             assertEquals(inventory.getItemCount(items), 0);
         }
-        
+
         for (String items: notElligible) {
             assertEquals(inventory.getItemCount(items), -1);
         }
     }
-    
-    
+
+
 
 }
